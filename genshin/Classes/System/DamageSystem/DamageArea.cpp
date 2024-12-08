df1056a0c7cad532a75e778c1734d457e0cf8715
kiriko <2750469650@qq.com>
2024-12-08 17:11:53 +0800

    增加了延迟造成伤害的功能，修复了点小错误

--- genshin/Classes/System/DamageSystem/DamageArea.cpp
+++ genshin/Classes/System/DamageSystem/DamageArea.cpp
@@ -7,26 +7,28 @@ USING_NS_CC;
 // 构造函数，初始化伤害区域
 DamageArea::DamageArea(DamageAreaType type, const cocos2d::Vec2& pos, float dmg, Element dmgType, bool applyEffect,
     Player* player, // 传入玩家指针
-    float rad, const cocos2d::Size& sz, const cocos2d::Vec2& start, const cocos2d::Vec2& end)
+    float rad, const cocos2d::Size& sz, const cocos2d::Vec2& start, const cocos2d::Vec2& end, float delayTime)
     : areaType(type), position(pos), damageAmount(dmg), damageType(dmgType), applyAttributesEffect(applyEffect),
-    player(player), radius(rad), size(sz), startPoint(start), endPoint(end) {
+    player(player), radius(rad), size(sz), startPoint(start), endPoint(end), delay(delayTime), elapsed(0.0f) {
 }
 
 // 判定是否与玩家发生碰撞并造成伤害
-void DamageArea::checkDamage() {
-    // 如果玩家对象为空，则不进行伤害计算
-    if (!player) {
-        return;
+void DamageArea::checkDamage(float deltaTime) {
+    elapsed += deltaTime; // 增加已用时间
+
+    if (elapsed < delay) {
+        return; // 如果未到延迟时间，直接返回
     }
 
-    // 判断区域类型，并根据区域类型执行不同的碰撞检测
+    if (!player) {
+        return; // 如果玩家对象为空，则跳过
+    }
+
     switch (areaType) {
     case DamageAreaType::CIRCLE:
         if (position.distance(player->getPosition()) <= radius) {
-            // 玩家在圆形区域内，造成伤害
             float finalDamage = applyAttributesEffect(player, damageAmount);
             player->takeDamage(finalDamage);
-            triggerDamageAnimation(player);
         }
         break;
 
@@ -35,19 +37,15 @@ void DamageArea::checkDamage() {
             player->getPosition().x <= position.x + size.width / 2 &&
             player->getPosition().y >= position.y - size.height / 2 &&
             player->getPosition().y <= position.y + size.height / 2) {
-            // 玩家在矩形区域内，造成伤害
             float finalDamage = applyAttributesEffect(player, damageAmount);
             player->takeDamage(finalDamage);
-            triggerDamageAnimation(player);
         }
         break;
 
     case DamageAreaType::LINE:
         if (distancePointToLine(player->getPosition(), startPoint, endPoint) <= radius) {
-            // 玩家在线性区域内，造成伤害
             float finalDamage = applyAttributesEffect(player, damageAmount);
             player->takeDamage(finalDamage);
-            triggerDamageAnimation(player);
         }
         break;
 
@@ -55,6 +53,7 @@ void DamageArea::checkDamage() {
         break;
     }
 }
+
 
 // 计算点到直线的距离
 float DamageArea::distancePointToLine(const cocos2d::Vec2& point, const cocos2d::Vec2& lineStart, const cocos2d::Vec2& lineEnd) {
@@ -68,12 +67,12 @@ float DamageArea::distancePointToLine(const cocos2d::Vec2& point, const cocos2d:
 }
 
 // 根据属性调整伤害并附加效果
-void DamageArea::applyAttributesEffect(Player* player, float baseDamage) {
+float DamageArea::applyAttributesEffect(Player* player, float baseDamage) {
     if (applyAttributesEffect) {
         float elementModifier = calculateElementalDamageModifier(damageType, player.getElement());
-        player.takeDamage(elementModifier * baseDamage);
+        baseDamage=elementModifier * baseDamage;
         player.applyElementalEffects(player, damageType)
     }
 
-    player.takeDamage(elementModifier * baseDamage);
+    return baseDamage;
 }
--- genshin/Classes/System/DamageSystem/DamageArea.h
+++ genshin/Classes/System/DamageSystem/DamageArea.h
@@ -3,6 +3,7 @@
 
 #include "../Common/Entities/Player/Player.h"
 #include "../Common/Element/Element.h"
+#include "cocos2d.h"
 
 enum class DamageAreaType {
     CIRCLE,
@@ -10,7 +11,7 @@ enum class DamageAreaType {
     LINE
 };
 
-class DamageArea {
+class DamageArea : public cocos2d::Node {
 private:
     DamageAreaType areaType;               // 区域类型（圆形、矩形、直线）
     cocos2d::Vec2 position;                // 区域的中心位置
@@ -21,13 +22,15 @@ private:
     Element damageType;                    // 伤害类型
     bool applyAttributesEffect;            // 是否根据元素属性调整伤害
     Player* player;                        // 玩家指针
-
+    float delay;                           // 延迟时间
+    float elapsed;                         // 已用时间
+    // 还可以添加动画id
 public:
     // 构造函数，初始化伤害区域
     DamageArea(DamageAreaType type, const cocos2d::Vec2& pos, float dmg, Element dmgType, bool applyEffect,
         Player* player, // 传入玩家指针
         float rad = 0, const cocos2d::Size& sz = cocos2d::Size::ZERO,
-        const cocos2d::Vec2& start = cocos2d::Vec2::ZERO, const cocos2d::Vec2& end = cocos2d::Vec2::ZERO);
+        const cocos2d::Vec2& start = cocos2d::Vec2::ZERO, const cocos2d::Vec2& end = cocos2d::Vec2::ZERO, float delayTime);
 
     // 判定是否与目标发生碰撞并造成伤害
     void checkDamage();
