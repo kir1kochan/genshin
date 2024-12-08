1a847e26263376f73ff174ce8ba18cc3457cc282
Vestigor <3158922730@qq.com>
2024-12-08 16:57:47 +0800

    重新绘制了地图

---
+++ genshin/Resources/maps/terrain_map_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="terrain_map_1" tilewidth="32" tileheight="32" tilecount="8000" columns="16">
+ <image source="../images/terrain_map_1.png" width="512" height="16000"/>
+</tileset>
---
+++ genshin/Resources/maps/terrain_map_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="terrain_map_2" tilewidth="32" tileheight="32" tilecount="7744" columns="16">
+ <image source="../images/terrain_map_2.png" width="512" height="15488"/>
+</tileset>
