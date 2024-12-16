5c66b7c934e6fcab21e7753b3cfc04c11e6c03f0
iAcane0963 <2352494@tongji.edu.cn>
2024-12-17 00:44:10 +0800

    添加了资源

--- genshin/Resources/maps/upholstery.tsx
+++ genshin/Resources/maps/upholstery.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="upholstery" tilewidth="32" tileheight="32" tilecount="512" columns="16">
- <image source="C:/Users/Acane/Downloads/upholstery.png" width="512" height="1024"/>
+ <image source="../images/upholstery.png" width="512" height="1024"/>
  <tile id="48" probability="2000">
   <properties>
    <property name="大小" type="int" value="10"/>
