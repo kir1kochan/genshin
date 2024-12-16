5121eecbb0dae47799f7ab6a8dcc5040a2f681fb
iAcane0963 <161813218+iAcane0963@users.noreply.github.com>
2024-12-16 23:55:52 +0800

    地图绘制

---
+++ genshin/Resources/maps/floors.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="floors" tilewidth="32" tileheight="32" tilecount="2048" columns="32">
+ <image source="lpc-floors/lpc-floors/floors.png" width="1024" height="2048"/>
+ <tile id="1062">
+  <objectgroup draworder="index" id="7">
+   <object id="8" x="0.181818" y="0.363636" width="31.4545" height="31.2727"/>
+  </objectgroup>
+ </tile>
+ <tile id="1447">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.363636" y="0" width="31.2727" height="32.1818"/>
+  </objectgroup>
+ </tile>
+</tileset>
