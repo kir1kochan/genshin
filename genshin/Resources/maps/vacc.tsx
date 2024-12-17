12623144784c8c12d140995a1c33a11a67a8cd12
Vestigor <3158922730@qq.com>
2024-12-17 18:15:45 +0800

    完善了世界地图

---
+++ genshin/Resources/maps/vacc.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="vacc" tilewidth="32" tileheight="32" tilecount="2560" columns="32">
+ <image source="../images/victorian-accessories.png" width="1024" height="2560"/>
+</tileset>
---
+++ genshin/Resources/maps/vwindoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="vwindoor" tilewidth="32" tileheight="32" tilecount="5120" columns="32">
+ <image source="../images/victorian-windows-doors.png" width="1024" height="5120"/>
+</tileset>
