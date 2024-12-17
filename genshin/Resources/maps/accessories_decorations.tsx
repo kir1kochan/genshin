683e97f310f60ae841abf328503ac8dc459aca3d
Vestigor <3158922730@qq.com>
2024-12-18 03:21:40 +0800

    完善了地图，存在一个图层无法显示，对象层未修改完

---
+++ genshin/Resources/maps/accessories_decorations.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="accessories_decorations" tilewidth="32" tileheight="32" tilecount="4480" columns="32">
+ <image source="../images/accessories_decorations.png" width="1024" height="4488"/>
+</tileset>
---
+++ genshin/Resources/maps/trees_rock.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="trees_rock" tilewidth="32" tileheight="32" tilecount="3520" columns="32">
+ <image source="../images/trees_rock.png" width="1024" height="3520"/>
+</tileset>
