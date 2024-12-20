2343905a75c9684a9cb00c404932a17bf8bdfd0b
Vestigor <3158922730@qq.com>
2024-12-20 09:50:08 +0800

    添加了碰撞区域和部分碰撞层

---
+++ genshin/Resources/maps/area.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="area" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <properties>
+  <property name="collide" value=""/>
+ </properties>
+ <image source="../images/area.png" width="192" height="128"/>
+ <tile id="15">
+  <properties>
+   <property name="collide" value="true"/>
+  </properties>
+ </tile>
+</tileset>
