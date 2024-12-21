2f31c48d2d9344fee0c507c5c49359965f1663c1
iAcane0963 <161813218+iAcane0963@users.noreply.github.com>
2024-12-21 23:12:16 +0800

    雾

---
+++ genshin/Resources/maps/fog01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="fog01" tilewidth="32" tileheight="32" tilecount="512" columns="32">
+ <image source="../images/fog01.png" width="1024" height="512"/>
+</tileset>
