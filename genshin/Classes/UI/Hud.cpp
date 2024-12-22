809ed902f1b185bc03f05614123fe447af946d19
iAcane0963 <161813218+iAcane0963@users.noreply.github.com>
2024-12-22 23:04:38 +0800

    Hud整合

--- genshin/Classes/UI/Hud.cpp
+++ genshin/Classes/UI/Hud.cpp
@@ -1,6 +1,7 @@
 #include "Hud.h"
 #include "Classes/Common/Entities/Player/Player.h"
 #include "TestScene.h"
+#include "Scene/SceneObject/TPAnchor.h"
 
 // 构造函数，初始化各成员变量
 Hud::Hud(Player* player)
@@ -9,6 +10,23 @@ Hud::Hud(Player* player)
     // 创建 DrawNode 用于显示血条和体力条
     healthBarNode = cocos2d::DrawNode::create();
     staminaBarNode = cocos2d::DrawNode::create();
+
+    // 初始化技能栏节点
+    for (int i = 0; i < 4; ++i) {
+        skillBarNode.push_back(cocos2d::DrawNode::create());
+        this->addChild(skillBarNode[i]);
+
+        // 初始化技能冷却进度条（蒙版）
+        cocos2d::Sprite* cooldownSprite = cocos2d::Sprite::create("imageSkill/Covery.jpg");
+        skillCooldownBars.push_back(cocos2d::ProgressTimer::create(cooldownSprite));
+        skillCooldownBars[i]->setType(cocos2d::ProgressTimer::Type::RADIAL);
+        skillCooldownBars[i]->setMidpoint(cocos2d::Vec2(0.5f, 0.5f));  // 设置为环形进度条
+        skillCooldownBars[i]->setBarChangeRate(cocos2d::Vec2(1, 1));
+        this->addChild(skillCooldownBars[i]);
+
+        // 初始隐藏冷却进度条
+        skillCooldownBars[i]->setVisible(false);
+    }
 
     // 将血条和体力条节点添加为子节点
     addChild(healthBarNode, -1);
@@ -58,7 +76,164 @@ Hud::Hud(Player* player)
     updateMiniMapPosition();
 
 
+    // 设置技能栏边框
+    for (int i = 0; i < skillBarNode.size(); ++i) {
+        skillBarNode[i]->drawRect(cocos2d::Vec2(0, 0), cocos2d::Vec2(50, 50), cocos2d::Color4F::BLACK);
+    }
+
+
+    // 初始化进度条的相对位置
+    updateBarPositions();
+
+    // 注册事件监听
+    auto listener2 = cocos2d::EventListenerCustom::create("MAP_ENDED_EVENT", [this](cocos2d::EventCustom* event) {
+        CCLOG("Map event ended!");
+        if (isMiniMapExpanded) {
+            toggleMiniMap();
+        }
+        });
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(listener2, this);  // 将监听器添加到事件分发器
 }
+
+void Hud::setTPAnchor(TPAnchor* tpAnchor) {
+    this->tpAnchor = tpAnchor;
+    float scale = 32.0f / 256.0f;
+    auto tpAnchors = tpAnchor->gettpPointActivation();
+    miniMapNode->removeChildByName("Anchor");
+    for (auto& anchor : tpAnchors) {
+        Vec2 pos = anchor.first;
+        if (anchor.second) {
+            auto sprite = cocos2d::Sprite::create("Icon/Anchor.jpg");
+            sprite->setScale(0.5);
+            miniMapNode->addChild(sprite);
+            sprite->setName("Anchor");
+            sprite->setPosition(pos.x * scale, pos.y * scale);
+        }
+        else {
+            auto sprite = cocos2d::Sprite::create("Icon/Anchor.jpg");
+            sprite->setScale(0.5);
+            miniMapNode->addChild(sprite);
+            sprite->setName("Anchor");
+            sprite->setColor(cocos2d::Color3B(128, 128, 128));
+            sprite->setPosition(pos.x * scale, pos.y * scale);
+        }
+    }
+}
+
+void Hud::updateMissionIconPosition(int stage) {
+    if (!expandedMiniMapNode) {
+        return; // 如果小地图2未加载，则直接返回
+    }
+    // 获取 mission 图层
+    auto missionLayer = expandedMiniMapNode->getObjectGroup("mission");
+    if (!missionLayer) {
+        return; // 如果 mission 图层不存在，则直接返回
+    }
+    // 获取主线任务图标对象
+    auto missionObject = missionLayer->getObject("MissionIcon");
+    if (missionObject.empty()) {
+        return; // 如果主线任务图标对象不存在，则直接返回
+    }
+    // 获取当前图标位置
+    float x = missionObject["x"].asFloat();
+    float y = missionObject["y"].asFloat();
+    // 根据阶段值更新图标位置
+    switch (stage) {
+    case 0:
+        x -= 200;
+        break;
+    case 1:
+        x += 300;
+        y += 300;
+        break;
+        // 可以根据需要添加更多阶段
+    default:
+        break;
+    }
+    // 更新主线任务图标的位置
+    auto missionIcon = expandedMiniMapNode->getChildByName<cocos2d::Sprite*>("MissionIcon");
+    if (missionIcon) {
+        missionIcon->setPosition(cocos2d::Vec2(x, y));
+    }
+    else {
+        // 如果图标不存在，则创建并添加到小地图2中
+        missionIcon = cocos2d::Sprite::create("mission_icon.png");
+        missionIcon->setName("MissionIcon");
+        missionIcon->setPosition(cocos2d::Vec2(x, y));
+        expandedMiniMapNode->addChild(missionIcon);
+    }
+}
+
+void Hud::updateSideMissionIconPosition(const std::string& missionName, bool isVisible) {
+    if (!expandedMiniMapNode) {
+        return; // 如果小地图2未加载，则直接返回
+    }
+    // 获取 sideMission 图层
+    auto sideMissionLayer = expandedMiniMapNode->getObjectGroup("sideMission");
+    if (!sideMissionLayer) {
+        return; // 如果 sideMission 图层不存在，则直接返回
+    }
+    // 获取支线任务图标对象
+    auto missionObject = sideMissionLayer->getObject(missionName);
+    if (missionObject.empty()) {
+        return; // 如果支线任务图标对象不存在，则直接返回
+    }
+    // 获取当前图标位置
+    float x = missionObject["x"].asFloat();
+    float y = missionObject["y"].asFloat();
+    // 更新支线任务图标的位置和可见性
+    auto missionIcon = expandedMiniMapNode->getChildByName<cocos2d::Sprite*>(missionName);
+    if (missionIcon) {
+        missionIcon->setVisible(isVisible);
+        if (isVisible) {
+            missionIcon->setPosition(cocos2d::Vec2(x, y));
+        }
+    }
+    else if (isVisible) {
+        // 如果图标不存在且需要显示，则创建并添加到小地图2中
+        missionIcon = cocos2d::Sprite::create("side_mission_icon.png");
+        missionIcon->setName(missionName);
+        missionIcon->setPosition(cocos2d::Vec2(x, y));
+        expandedMiniMapNode->addChild(missionIcon);
+    }
+}
+cocos2d::TMXTiledMap* Hud::getMiniMapNode() const {
+    return miniMapNode;
+}
+cocos2d::TMXTiledMap* Hud::getExpandedMiniMapNode() const {
+    return expandedMiniMapNode;
+}
+
+
+void Hud::hideFogLayers(cocos2d::TMXTiledMap* map, int index) {
+    std::string layerName = "fog_" + std::to_string(index);
+    auto layer = map->getLayer(layerName);
+    if (layer) {
+        layer->setVisible(false);
+    }
+    float scale = 32.0f / 256.0f;
+    auto tpAnchors = tpAnchor->gettpPointActivation();
+    miniMapNode->removeChildByName("Anchor");
+    for (auto& anchor : tpAnchors) {
+        Vec2 pos = anchor.first;
+        if (anchor.second) {
+            auto sprite = cocos2d::Sprite::create("Icon/Anchor.jpg");
+            sprite->setScale(0.5);
+            miniMapNode->addChild(sprite);
+            sprite->setName("Anchor");
+            sprite->setPosition(pos.x * scale, pos.y * scale);
+        }
+        else {
+            auto sprite = cocos2d::Sprite::create("Icon/Anchor.jpg");
+            sprite->setScale(0.5);
+            miniMapNode->addChild(sprite);
+            sprite->setName("Anchor");
+            sprite->setColor(cocos2d::Color3B(128, 128, 128));
+            sprite->setPosition(pos.x * scale, pos.y * scale);
+        }
+    }
+}
+
 
 // 初始化小地图玩家图标
 void Hud::initMiniMapPlayerIcon() {
@@ -123,6 +298,66 @@ void Hud::updateStaminaBar(float staminaPercent) {
     drawProgressBar(staminaBarNode, staminaPercent, cocos2d::Color4F::GREEN);
 }
 
+// 装备技能
+void Hud::equipSkill(int skillSlot, std::string skillName) {
+    // 为技能槽装备技能
+    std::string iconPath = "imageSkill/" + skillName + ".png";
+    cocos2d::Sprite* skillIcon = cocos2d::Sprite::create(iconPath);
+
+    if (skillIcon) {
+        // 获取技能栏的位置
+        cocos2d::Vec2 skillBarPosition = skillBarNode[skillSlot]->getPosition();
+
+        // 设置技能图标的大小（使图标适应50x50的边框）
+        skillIcon->setScale(50.0f / skillIcon->getContentSize().width, 50.0f / skillIcon->getContentSize().height);
+
+        // 设置技能图标的锚点为右上角
+        skillIcon->setAnchorPoint(cocos2d::Vec2(0, 0));  // 锚点设为中心对齐
+
+        // 将技能图标设置为居中在技能栏边框内
+        skillIcon->setPosition(skillBarPosition);
+
+        // 添加技能图标到 HUD 中
+        this->addChild(skillIcon);
+
+        // 确保技能图标在进度条上方
+        skillIcon->setLocalZOrder(0);  // 将技能图标置于上方
+    }
+
+    // 显示技能冷却条蒙版
+    skillCooldownBars[skillSlot]->setVisible(true);
+
+    // 设置冷却进度条的尺寸与技能图标一致（50x50）
+    skillCooldownBars[skillSlot]->setScale(6.45f / skillIcon->getContentSize().width, 6.45f / skillIcon->getContentSize().height);
+
+    skillCooldownBars[skillSlot]->setOpacity(150);
+    skillCooldownBars[skillSlot]->setAnchorPoint(cocos2d::Vec2(0, 0));  // 锚点设为中心对齐
+    // 设置冷却进度条位置与技能图标一致
+    skillCooldownBars[skillSlot]->setPosition(skillBarNode[skillSlot]->getPosition());
+
+    // 确保冷却进度条在技能图标下方（设置较低的 z-index）
+    skillCooldownBars[skillSlot]->setLocalZOrder(1);  // 进度条在技能图标下方
+}
+
+// 卸载技能
+void Hud::unequipSkill(int skillSlot) {
+    // 卸载技能图标
+    skillBarNode[skillSlot]->removeAllChildren(); // 清除技能图标
+    skillCooldownBars[skillSlot]->setVisible(false); // 隐藏技能冷却进度条
+}
+
+// 使用技能
+void Hud::useSkill(int skillSlot, float cdTime) {
+    skillCooldownBars[skillSlot]->setLocalZOrder(2);  // 进度条在技能图标下方
+    // 使用技能时启动冷却进度条
+    skillCooldownBars[skillSlot]->setPercentage(100); // 初始化为100%
+
+    // 动态更新冷却进度条
+    cocos2d::Action* cooldownAction = cocos2d::ProgressTo::create(2 * cdTime, 0);
+    skillCooldownBars[skillSlot]->runAction(cooldownAction);
+    skillCooldownBars[skillSlot]->setLocalZOrder(0);  // 进度条在技能图标下方
+}
+
 // 绘制进度条
 void Hud::drawProgressBar(cocos2d::DrawNode* barNode, float percent, cocos2d::Color4F color) {
     barNode->clear(); // 清空旧的内容
@@ -139,6 +374,7 @@ void Hud::drawProgressBar(cocos2d::DrawNode* barNode, float percent, cocos2d::Co
     cocos2d::Vec2 topRight(barWidth, barHeight); // 右上角点
     barNode->drawRect(origin, topRight, cocos2d::Color4F::BLACK); // 黑色边框
 }
+
 
 // 设置进度条的相对位置
 void Hud::updateBarPositions() {
@@ -161,7 +397,11 @@ void Hud::updateMiniMapPosition() {
 }
 
 void Hud::toggleMiniMap() {
+
+
     if (isMiniMapExpanded) {
+
+        player->getChildByName("sprite")->setVisible(true);
         // 关闭新加载的小地图
         if (expandedMiniMapNode) {
             expandedMiniMapNode->removeFromParent();
@@ -175,6 +415,7 @@ void Hud::toggleMiniMap() {
         isMiniMapExpanded = false;
     }
     else {
+        player->getChildByName("sprite")->setVisible(false);
         // 隐藏原有的小地图
         miniMapNode->setVisible(false);
         clipper->setVisible(false); // 隐藏裁剪节点
@@ -189,6 +430,7 @@ void Hud::toggleMiniMap() {
 
         // 初始化新的小地图玩家图标
         auto expandedMiniMapPlayerIcon = cocos2d::Sprite::create("player.png");
+        expandedMiniMapPlayerIcon->setScale(0.5);
         expandedMiniMapNode->addChild(expandedMiniMapPlayerIcon);
 
         // 获取小地图的 Objects 层和 SpawnPoint 对象
@@ -204,99 +446,37 @@ void Hud::toggleMiniMap() {
         float scale = 32.0f / 256.0f;
         auto playerPos = player->getPosition();
         expandedMiniMapPlayerIcon->setPosition(playerPos.x * scale, playerPos.y * scale);
-
-
+        auto tpAnchors = tpAnchor->gettpPointActivation();
+        expandedMiniMapNode->removeChildByName("Anchor");
+        for (auto& anchor : tpAnchors) {
+            Vec2 pos = anchor.first;
+            if (anchor.second) {
+                auto button = cocos2d::MenuItemImage::create(
+                    "Icon/Anchor.jpg",
+                    "Icon/Anchor.jpg",
+                    [this, pos](cocos2d::Ref* sender) {
+                        player->setPosition(pos);
+                        CCLOG(("touched!"));
+                    });
+                auto menu = cocos2d::Menu::create(button, nullptr);
+                menu->setPosition(cocos2d::Vec2::ZERO);
+                menu->setName("Anchor");
+                expandedMiniMapNode->addChild(menu);
+                button->setPosition(pos.x * scale, pos.y * scale);
+            }
+            else {
+                auto sprite = cocos2d::Sprite::create("Icon/Anchor.jpg");
+                expandedMiniMapNode->addChild(sprite);
+                sprite->setName("Anchor");
+                sprite->setColor(cocos2d::Color3B(128, 128, 128));
+                sprite->setPosition(pos.x * scale, pos.y * scale);
+            }
+        }
         isMiniMapExpanded = true;
     }
 }
 
 
-void Hud::updateMissionIconPosition(int stage) {
-    if (!expandedMiniMapNode) {
-        return; // 如果小地图2未加载，则直接返回
-    }
-
-    // 获取 mission 图层
-    auto missionLayer = expandedMiniMapNode->getObjectGroup("mission");
-    if (!missionLayer) {
-        return; // 如果 mission 图层不存在，则直接返回
-    }
-
-    // 获取主线任务图标对象
-    auto missionObject = missionLayer->getObject("MissionIcon");
-    if (missionObject.empty()) {
-        return; // 如果主线任务图标对象不存在，则直接返回
-    }
-
-    // 获取当前图标位置
-    float x = missionObject["x"].asFloat();
-    float y = missionObject["y"].asFloat();
-
-    // 根据阶段值更新图标位置
-    switch (stage) {
-    case 0:
-        x -= 200;
-        break;
-    case 1:
-        x += 300;
-        y += 300;
-        break;
-        // 可以根据需要添加更多阶段
-    default:
-        break;
-    }
-
-    // 更新主线任务图标的位置
-    auto missionIcon = expandedMiniMapNode->getChildByName<cocos2d::Sprite*>("MissionIcon");
-    if (missionIcon) {
-        missionIcon->setPosition(cocos2d::Vec2(x, y));
-    }
-    else {
-        // 如果图标不存在，则创建并添加到小地图2中
-        missionIcon = cocos2d::Sprite::create("mission_icon.png");
-        missionIcon->setName("MissionIcon");
-        missionIcon->setPosition(cocos2d::Vec2(x, y));
-        expandedMiniMapNode->addChild(missionIcon);
-    }
-}
-
-void Hud::updateSideMissionIconPosition(const std::string& missionName, bool isVisible) {
-    if (!expandedMiniMapNode) {
-        return; // 如果小地图2未加载，则直接返回
-    }
-
-    // 获取 sideMission 图层
-    auto sideMissionLayer = expandedMiniMapNode->getObjectGroup("sideMission");
-    if (!sideMissionLayer) {
-        return; // 如果 sideMission 图层不存在，则直接返回
-    }
-
-    // 获取支线任务图标对象
-    auto missionObject = sideMissionLayer->getObject(missionName);
-    if (missionObject.empty()) {
-        return; // 如果支线任务图标对象不存在，则直接返回
-    }
-
-    // 获取当前图标位置
-    float x = missionObject["x"].asFloat();
-    float y = missionObject["y"].asFloat();
-
-    // 更新支线任务图标的位置和可见性
-    auto missionIcon = expandedMiniMapNode->getChildByName<cocos2d::Sprite*>(missionName);
-    if (missionIcon) {
-        missionIcon->setVisible(isVisible);
-        if (isVisible) {
-            missionIcon->setPosition(cocos2d::Vec2(x, y));
-        }
-    }
-    else if (isVisible) {
-        // 如果图标不存在且需要显示，则创建并添加到小地图2中
-        missionIcon = cocos2d::Sprite::create("side_mission_icon.png");
-        missionIcon->setName(missionName);
-        missionIcon->setPosition(cocos2d::Vec2(x, y));
-        expandedMiniMapNode->addChild(missionIcon);
-    }
-}
 
 cocos2d::TMXTiledMap* Hud::getMiniMapNode() const {
     return miniMapNode;
--- genshin/Classes/UI/Hud.h
+++ genshin/Classes/UI/Hud.h
@@ -2,7 +2,9 @@
 #define HUD_H
 
 #include "cocos2d.h"
+#include <vector>
 
+class TPAnchor;
 class Player;
 
 class Hud : public cocos2d::Node {
@@ -12,19 +14,24 @@ private:
     cocos2d::DrawNode* staminaBarNode; // 体力条节点
     cocos2d::TMXTiledMap* miniMapNode; // 小地图节点
     cocos2d::Sprite* miniMapPlayerIcon; // 小地图玩家图标
-    cocos2d::ClippingNode* clipper;//裁剪
-    cocos2d::DrawNode* stencil;//遮罩层
-    cocos2d::DrawNode* borderNode;//边框
-    cocos2d::TMXTiledMap* expandedMiniMapNode ;
+    cocos2d::ClippingNode* clipper; // 裁剪
+    cocos2d::DrawNode* stencil; // 遮罩层
+    cocos2d::DrawNode* borderNode; // 边框
+    cocos2d::TMXTiledMap* expandedMiniMapNode;
     cocos2d::Sprite* expandedMiniMapPlayerIcon;
 
+    // 技能栏绘制节点
+    std::vector<cocos2d::DrawNode*> skillBarNode;   // 技能栏节点
+
+    // 冷却罩子（进度条）
+    std::vector<cocos2d::ProgressTimer*> skillCooldownBars;  // 冷却进度条
 
     // 进度条的宽度和高度
     float barWidth;  // 进度条的总宽度
     float barHeight; // 进度条的高度
 
-    // 引用玩家对象
-    Player* player;
+    TPAnchor* tpAnchor; // 用于传送点
+    Player* player;     // 引用玩家对象
 
     // 小地图显示状态
     bool isMiniMapExpanded;
@@ -43,6 +50,9 @@ public:
     Hud(Player* player);
     virtual ~Hud();
 
+    // 设置传送点
+    void setTPAnchor(TPAnchor* tpAnchor);
+
     // 静态创建方法
     static Hud* create(Player* player);
 
@@ -53,21 +63,34 @@ public:
     void updateHealthBar(float healthPercent);
     void updateStaminaBar(float staminaPercent);
 
+    // 装备技能
+    void equipSkill(int skillSlot, std::string skillName);
+    // 卸载技能
+    void unequipSkill(int skillSlot);
+    // 使用技能
+    void useSkill(int skillSlot, float cdTime);
+
     // 更新小地图的位置
     void updateMiniMapPosition();
-    //切换小地图显示方式
+    // 切换小地图显示方式
     void toggleMiniMap();
 
     // 更新主线任务图标位置
     void updateMissionIconPosition(int stage);
-    //支线
+    // 更新支线任务图标位置
     void updateSideMissionIconPosition(const std::string& missionName, bool isVisible);
 
-    //隐藏雾
+    // 隐藏雾层
+    void hideFogLayers(cocos2d::TMXTiledMap* map);
     void hideFogLayers(cocos2d::TMXTiledMap* map, int index);
 
+    // 获取小地图节点
     cocos2d::TMXTiledMap* getMiniMapNode() const;
+    // 获取扩展的小地图节点
     cocos2d::TMXTiledMap* getExpandedMiniMapNode() const;
+
+    // 获取小地图显示状态
+    bool getIsExpanded() const;
 
     // 设置悬浮小地图的可见性
     void setMiniMapVisible(bool visible);
